fb9e566d07ad33e32b357dab2f102f5b9713c140
AishaZhenisbekqz <117504731+AishaZhenisbekqz@users.noreply.github.com>
2022-11-12 11:06:46 +0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,10 @@
+sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:wsgi_application
+sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
 sudo rm /etc/nginx/sites-enabled/test.conf
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:wsgi_application
-sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
 
 sudo /etc/init.d/mysql restart
 mysql -u root -e "CREATE DATABASE qa CHARACTER SET utf8"
